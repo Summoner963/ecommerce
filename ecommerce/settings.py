e014a5a5d2ff8083f9716dc456027e2ebc1b3b37
Suman Dangal <lagzap9@gmail.com>
2026-04-01 10:06:29 +0545

    changes for switching from sqlite to postgres

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'whitenoise.runserver_nostatic',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -62,11 +63,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',  # Local development fallback
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
